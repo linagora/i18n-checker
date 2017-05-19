e339367a15dbe2eb357054d6234e429e2d57534c
Sang NGO DOAN <sngodoan@linagora.com>
2017-05-19 10:28:14 +0700

    OR-2633 Now able to detect keys in exclamation mark syntax

--- test/utils/jade/jade.js
+++ test/utils/jade/jade.js
@@ -3,8 +3,23 @@ const path = require('path');
 const lib = require('../../../src/utils/jade');
 
 describe('The utils/jade module', function() {
-  it('should extract all i18n keys in the jade file', function() {
-    const keys = lib.extractKeysInFile(path.join(__dirname, 'fixture/all.jade'));
+  it('should extract all i18n keys in the jade file (sharp syntax)', function() {
+    const keys = lib.extractKeysInFile(path.join(__dirname, 'fixture/sharp-syntax.jade'));
+
+    expect(keys).to.deep.equal([
+      'Your account %s is currently unavailable.',
+      'You can click to retry',
+      'Hahaha',
+      'Confirmation',
+      'Remove "%s" and all events it contains?',
+      'Remove \\"%s\\" and all events it contains?',
+      'I',
+      'Delete'
+    ]);
+  });
+
+  it('should extract all i18n keys in the jade file (exclamation mark syntax)', function() {
+    const keys = lib.extractKeysInFile(path.join(__dirname, 'fixture/exclamation-mark-syntax.jade'));
 
     expect(keys).to.deep.equal([
       'Your account %s is currently unavailable.',
